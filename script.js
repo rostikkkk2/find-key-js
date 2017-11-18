a0ce0343c9aac03ed322d4c63a3f4f23ee85b714
Rostik Yuriev <rost4402255@gmail.com>
2017-11-18 15:03:39 +0200

    redone find-key

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function findKey() {
-  var obg_keys = {
-    name: "Rostik",
-    surname: "Yuriev",
-    age: 16
-  }
-  if (obg_keys.name && obg_keys.surname && obg_keys.age) {
-    return true;
-  }else {
-    return false;
-  }
 
+var obg_keys = {
+  name: "Rostik",
+  surname: "Yuriev",
+  age: 16
 }
-alert(findKey());
+
+function findKey(any_obg) {
+  if (any_obg != undefined && any_obg != "" && any_obg != null && any_obg != 0) {
+    if (any_obg.name && any_obg.surname && any_obg.age) {
+      return true;
+    }
+  }
+  return false;
+}
+alert(findKey(obg_keys));
