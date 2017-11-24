d88746efe75d4101f7f8fd57bb8dd5244bfa8fb5
Rostik Yuriev <rost4402255@gmail.com>
2017-11-24 19:38:32 +0200

    redone find key

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-
 var obg_keys = {
   name: "Rostik",
   surname: "Yuriev",
-  age: 16
+  age: 16,
 }
 
 function findKey(any_obg) {
-  if (any_obg != undefined && any_obg != "" && any_obg != null && any_obg != 0) {
-    if (any_obg.name && any_obg.surname && any_obg.age) {
-      return true;
+  var arr_keys = Object.keys(any_obg);
+  var res = [];
+    for (var i = 0; i < arr_keys.length; i++) {
+      if (arr_keys[i] == "name" || arr_keys[i] == "surname" || arr_keys[i] == "age") {
+        res.push(arr_keys[i]);
     }
   }
-  return false;
+    return arr_keys.length == res.length;
 }
+
 alert(findKey(obg_keys));
